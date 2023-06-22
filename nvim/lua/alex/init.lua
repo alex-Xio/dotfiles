894fe1826fe341162c098e60507eafe0b9c3da8f
Александр Андриюк <alexander@iMac-Aleksandr.local>
2023-06-22 11:36:06 +0300

    Batman! (this commit has no parents)

---
+++ nvim/lua/alex/init.lua
@@ -0,0 +1,3 @@
+require("alex.remap")
+require("alex.lazy")
+require("alex.set")
---
+++ nvim/lua/alex/lazy.lua
@@ -0,0 +1,50 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = " " 
+
+require("lazy").setup({
+  "ThePrimeagen/vim-be-good",
+  {
+  	  "folke/tokyonight.nvim",
+  	  lazy = false,
+  	  priority = 1000,
+  },
+  {
+	  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate"
+  },
+  {
+    	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
+	  dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  "mbbill/undotree",
+  {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v2.x',
+  dependencies = {
+    -- LSP Support
+    {'neovim/nvim-lspconfig'},             -- Required
+    {                                      -- Optional
+      'williamboman/mason.nvim',
+      build = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+},
+})
